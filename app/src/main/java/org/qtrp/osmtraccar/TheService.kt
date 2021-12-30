aeba6c8a5adb254591ecddd5b15413a0ed07e6ef
dexterlb <dexterlb@qtrp.org>
2021-12-30 18:35:23 +0200

    fancy notification

--- app/src/main/java/org/qtrp/osmtraccar/TheService.kt
+++ app/src/main/java/org/qtrp/osmtraccar/TheService.kt
@@ -1,12 +1,13 @@
 package org.qtrp.osmtraccar
 
 import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import android.view.View
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,6 +23,7 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
         fun serviceLogMessage(level: Int, msg: String)
     }
 
+    private var notificationReceivers: MutableList<BroadcastReceiver> = mutableListOf()
     private var mEventListener: EventListener? = null
 
     private var startMode: Int = 0             // indicates how to behave if the service is killed
@@ -38,6 +40,8 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
         val NOTIFICATION_CHANNEL_PERSISTENT = "notification_channel_persistent"
 
         val NOTIFICATION_ID_PERSISTENT = 42
+
+        val ACTION_STOP_SERVICE = "action_stop_service"
     }
 
 
@@ -49,10 +53,12 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
         traccarApi = TraccarApi(this, this)
 
         pointShower.setOsmandInitActivity(activity)
+
         initOsmandApi()
     }
 
     private fun initOsmandApi() {
+        updateNotification("connecting to OsmAnd")
         pointShower.initOsmAndApi(this, osmandPackage)
         pointShower.clear()
     }
@@ -84,6 +90,7 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
     }
 
     fun traccarConnect() {
+        updateNotification("connecting to Traccar")
         val scope = CoroutineScope(Job() + Dispatchers.IO)
 
         scope.launch {
@@ -104,6 +111,7 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
     override fun traccarSocketConnectedState(isConnected: Boolean) {
         if (isConnected) {
             log(Log.INFO, "traccar connected")
+            updateNotification("connected")
         } else {
             log(Log.INFO, "traccar disconnected")
             pointShower.clear()
@@ -120,8 +128,12 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
     }
 
     override fun onCreate() {
-        // The service is being created
+        // Notification ID cannot be 0.
+        registerNotificationActions()
+        startForeground(NOTIFICATION_ID_PERSISTENT, makeNotification("starting"))
+    }
 
+    private fun makeNotification(text: String): Notification {
         createNotificationChannels()
 
         val pendingIntent: PendingIntent =
@@ -129,17 +141,43 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
                 PendingIntent.getActivity(this, 0, notificationIntent, 0)
             }
 
-        val notification: Notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_PERSISTENT)
-            .setContentTitle("ble ble")
-            .setContentText("working")
-            // .setSmallIcon(R.drawable.icon)
+        val stopActionIntent = PendingIntent.getBroadcast(
+            this,
+            0,
+            Intent(ACTION_STOP_SERVICE),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val stopAction = NotificationCompat.Action(R.drawable.osmtraccar_stop_icon, "stop", stopActionIntent)
+
+        return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_PERSISTENT)
+            .setContentTitle("Traccar -> OsmAnd")
+            .setContentText(text)
+            .setSmallIcon(R.drawable.osmtraccar_notif_icon)
             .setContentIntent(pendingIntent)
-            .setTicker("tick tick tick")
+            .setTicker(text)
+            .addAction(stopAction)
             .build()
+    }
 
-        // Notification ID cannot be 0.
-        startForeground(NOTIFICATION_ID_PERSISTENT, notification)
+    private fun registerNotificationActions() {
+        registerNotificationReceiver(object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    pleaseStop()
+                }
+            }, IntentFilter(ACTION_STOP_SERVICE)
+        )
+    }
 
+    private fun registerNotificationReceiver(rec: BroadcastReceiver, filter: IntentFilter) {
+        registerReceiver(rec, filter)
+        notificationReceivers.add(rec)
+    }
+    
+    private fun unregisterNotificationActions() {
+        for (rec in notificationReceivers) {
+            unregisterReceiver(rec)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -167,6 +205,7 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
     override fun onDestroy() {
         // The service is no longer used and is being destroyed
         TheServiceRunning.isRunning = false
+        unregisterNotificationActions()
         traccarApi.unsubscribePositionUpdates()
         pointShower.clear()
     }
@@ -176,6 +215,11 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
         mEventListener = eventListener
 
         log(Log.INFO, "hello from service")
+    }
+
+    private fun updateNotification(text: String) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID_PERSISTENT, makeNotification(text))
     }
 
     private fun log(priority: Int, msg: String) {
@@ -201,6 +245,7 @@ class TheService : Service(), OsmAndHelper.OsmandEventListener, TraccarEventList
             val descriptionText = "Persistent notification for long-running service"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val mChannel = NotificationChannel(NOTIFICATION_CHANNEL_PERSISTENT, name, importance)
+            mChannel.setSound(null, null)
             mChannel.description = descriptionText
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
